2b020d5fcfea5db2ae5f3c19da67b97b22d71438
KostyaLosev <losevkanstantin@gmail.com>
2025-04-05 14:27:16 +0300

    feat: completed task

---
+++ React-HW/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import FooterBrand from "./footerBrand/FooterBrand";
+import FooterLinks from "./footerLinks/FooterLinks";
+import FooterBottom from "./footerBottom/FooterBottom";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <FooterBrand />
+                <FooterLinks />
+            </div>
+            <FooterBottom text="Built by Flowbase• Powered by Webflow" />
+        </footer>
+    );
+};
+
+export default Footer;
