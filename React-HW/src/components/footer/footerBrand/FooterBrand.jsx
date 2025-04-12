7b8bc0a9d109d62b326be2fafefd85a4b17260d0
KostyaLosev <losevkanstantin@gmail.com>
2025-04-12 12:25:23 +0300

    fix: changed logo.png to svg

--- React-HW/src/components/footer/footerBrand/FooterBrand.jsx
+++ React-HW/src/components/footer/footerBrand/FooterBrand.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./footerBrand.module.css"
-import Logo from "../../../assets/Logo.png"
+import Logo from "../../../assets/Logo.svg"
 
 const FooterBrand = () => {
     return (
